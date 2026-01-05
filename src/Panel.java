5373fc8fec3eee70d5b1d5f2302f9e25f1edab69
Evan-Horn <evanhorn2006@gmail.com>
2026-01-05 06:12:03 -0500

    Fleshed out panel class a bit

--- src/Panel.java
+++ src/Panel.java
@@ -1,2 +1,146 @@
-public class Panel {
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.ComponentListener;
+import java.awt.event.ComponentEvent;
+import javax.swing.Timer;
+import java.awt.image.BufferedImage;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.Toolkit;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Point2D;
+public class Panel extends JPanel implements KeyListener, MouseListener, MouseMotionListener, ComponentListener{
+
+    private Rectangle2D map;
+    public Panel()
+    {
+
+    }
+    /**
+     * @param e the event to be processed
+     */
+    @Override
+    public void componentResized(ComponentEvent e) {
+
+    }
+
+    /**
+     * @param e the event to be processed
+     */
+    @Override
+    public void componentMoved(ComponentEvent e) {
+
+    }
+
+    /**
+     * @param e the event to be processed
+     */
+    @Override
+    public void componentShown(ComponentEvent e) {
+
+    }
+
+    /**
+     * @param e the event to be processed
+     */
+    @Override
+    public void componentHidden(ComponentEvent e) {
+
+    }
+
+    /**
+     * @param e the event to be processed
+     */
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    /**
+     * @param e the event to be processed
+     */
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    /**
+     * @param e the event to be processed
+     */
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    /**
+     * @param e the event to be processed
+     */
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    /**
+     * @param e the event to be processed
+     */
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    /**
+     * @param e the event to be processed
+     */
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    /**
+     * @param e the event to be processed
+     */
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    /**
+     * @param e the event to be processed
+     */
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    /**
+     * @param e the event to be processed
+     */
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    /**
+     * @param e the event to be processed
+     */
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+    private class Listener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) //evoked each iteration, basically its always being called as the program runs
+        {
+
+        }
+    }
 }
